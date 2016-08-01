1c9be3d716d0d9e549018ff10ec4e6d15be44e04
Henry Fang <henry.fang1216@frog-jump.com>
2016-08-01 14:31:32 +0800

    complete

--- Day1Homework/Day1Homework/Controllers/HomeController.cs
+++ Day1Homework/Day1Homework/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Day1Homework.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +12,18 @@ namespace Day1Homework.Controllers
     {
         public ActionResult Index()
         {
+            List<SelectListItem> items = GetSelectList();
+
+            ViewBag.CategoryItems = items;
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GridViewAction()
+        {
+            var models = GetData();
+
+            ViewBag.MyClassModels = models;
             return View();
         }
 
@@ -25,6 +39,26 @@ namespace Day1Homework.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private List<SelectListItem> GetSelectList()
+        {
+            var r = new List<SelectListItem>();
+            r.Add(new SelectListItem() { Value = "", Text = "請選擇", Selected = true });
+            r.Add(new SelectListItem() { Value = "1", Text = "支出" });
+            r.Add(new SelectListItem() { Value = "2", Text = "收入" });
+
+            return r;
+        }
+
+        private List<MyClassModel> GetData()
+        {
+            var r = new List<MyClassModel>();
+            r.Add(new MyClassModel() { Sn = "1", Category = "支出", Date = "2016-01-01", Amount = "300" });
+            r.Add(new MyClassModel() { Sn = "2", Category = "支出", Date = "2016-01-02", Amount = "1,600" });
+            r.Add(new MyClassModel() { Sn = "3", Category = "支出", Date = "2016-01-03", Amount = "800" });
+
+            return r;
         }
     }
 }
---
+++ Day1Homework/Day1Homework/Models/MyClassModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day1Homework.Models
+{
+    public class MyClassModel
+    {
+        public string Sn { get; set; }
+        public string Category { get; set; }
+        public string Date { get; set; }
+        public string Amount { get; set; }
+    }
+}
